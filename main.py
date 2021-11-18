9ba238bf60b8fce4169612062889edeedf660ac6
arthast <arsentakhunov@gmail.com>
2021-11-18 18:14:05 +0300

    add print Hallo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('Hallo')
+    # 99bda3f3299caf9d6fae27d9bc6685ab84c52049
 
 
 if __name__ == '__main__':
